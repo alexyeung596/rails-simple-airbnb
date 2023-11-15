e5fc588ab6f8d98f829a76961e9d0675e04f8d26
Alex Young <zitayeung596@gmail.com>
2023-11-15 18:49:16 +0000

    “update”

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+12.times do
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen, and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url: 'https://news.airbnb.com/wp-content/uploads/sites/4/2019/06/PJM020719Q202_Luxe_WanakaNZ_LivingRoom_0264-LightOn_R1.jpg?w=3200'
+)
+end
+
+
+# 5.times do |index|
+#   Restaurant.create(
+#     name: "Restaurant #{index}",
+#     address: "#{index} Sample Street",
+#     phone_number: "123-456-7890",
+#     category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+#   )
+# end
